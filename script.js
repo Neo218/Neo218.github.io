ea4f445c040833ed2987de6b101ddaa966fac76d
Neo218 <125832954+Neo218@users.noreply.github.com>
2023-02-28 18:15:23 -0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// Función para imprimir
-function printSection(id) {
-    var element = document.getElementById(id);
-    var popupWin = window.open('', '_blank', 'width=600,height=600');
-    popupWin.document.write('<html><head><link rel="stylesheet" type="text/css" href="style.css" /></head><body onload="window.print()">' + element.innerHTML + '</body></html>');
-    popupWin.document.close();
-  }
